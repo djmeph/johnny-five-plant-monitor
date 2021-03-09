396555fc03b8b3cc82e2eaba0f788b67a0f09ae9
DJ Meph <norm.witte@gmail.com>
2021-03-09 06:04:28 +0000

    Hello World Jumbotron

--- apps/plant-monitor/src/app/app.component.ts
+++ apps/plant-monitor/src/app/app.component.ts
@@ -1,13 +1,8 @@
 import { Component } from '@angular/core';
-import { HttpClient } from '@angular/common/http';
-import { Message } from '@johnny-five-plant-monitor/api-interfaces';
 
 @Component({
   selector: 'johnny-five-plant-monitor-root',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.scss'],
 })
-export class AppComponent {
-  hello$ = this.http.get<Message>('/api/hello');
-  constructor(private http: HttpClient) {}
-}
+export class AppComponent {}
