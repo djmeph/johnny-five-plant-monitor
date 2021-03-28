eb1b13826eafd4de5c39b46eec9da7916002f513
DJ Meph <norm.witte@gmail.com>
2021-03-28 04:14:10 +0000

    Setup mongoose in nest

--- apps/gateway/src/app/app.module.ts
+++ apps/gateway/src/app/app.module.ts
@@ -2,9 +2,17 @@ import { Module } from '@nestjs/common';
 
 import { AppGateway } from './app.gateway';
 import { BoardIoModule } from '@johnny-five-plant-monitor/board-io';
+import { MongooseModule } from '@nestjs/mongoose';
 
 @Module({
-  imports: [BoardIoModule],
+  imports: [
+    BoardIoModule,
+    MongooseModule.forRoot('mongodb://database:27017', {
+      dbName: 'plant-monitor',
+      user: process.env.MONGODB_USERNAME,
+      pass: process.env.MONGODB_PASSWORD,
+    }),
+  ],
   controllers: [],
   providers: [AppGateway],
 })
